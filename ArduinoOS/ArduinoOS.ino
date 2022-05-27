0fa1b97a3cd487e5eef7af5a8c0f29511838ef78
Klaas <k.ketelaar@gmx.com>
2022-05-27 16:12:54 +0200

    commandsProcessed but no filters

--- ArduinoOS/ArduinoOS.ino
+++ ArduinoOS/ArduinoOS.ino
@@ -1,62 +1,122 @@
 // C++ code
 //
 #define BUFSIZE 12
+int commandLength = 0;
+int structSize = 10;
+char commandName[BUFSIZE];
+
+
+void store();
+void retrieve();
+void erase();
+void files();
+void freespace();
+void run();
+void list();
+void suspend();
+void resume();
+void kill();
+
+
 typedef struct{
   char name[BUFSIZE];
   void (*func)();
 }commandType;
 
 
-void store(){
-  Serial.println("store functie aangeroepen");
-}
-void retrieve(){
-  Serial.println("retrieve functie aangeroepen");
-}
-
 static commandType command[] ={
   {"store", &store},
-  {"retrieve", &retrieve}
+  {"retrieve", &retrieve},
+  {"erase", &erase},
+  {"files", &files},
+  {"freespace", &freespace},
+  {"run", &run},
+  {"list", &list},
+  {"suspend", &suspend},
+  {"resume", &resume},
+  {"kill", &kill}
 };
 
 
 
 void setup()
 {
+
   Serial.begin(9600);
 }
 
 void loop()
 {
+  processCommand();
+  // do other stuff
+}
+
+void processCommand(){
     commandType commandType;
-    int CHARCOUNT = 0;
-    const char k = 'k';
     char c;
     while (Serial.available()) 
     {
       c = Serial.read();
-      if (c == ' ' || c == '\r' || c =='\n') 
-      {
-      //  static int n = sizeof(command)/sizeof(commandType);
-        Serial.println("in if statement:");
-//        strncat(commandType.name, "\0",n+1);
-       Serial.println(CHARCOUNT);
+      if (c == ' ' || c == '\r' || c =='\n') {
         Serial.println(commandType.name);
-       for(int i=0; i<CHARCOUNT; i++){
-        Serial.println("in for statement");
-        Serial.println(commandType.name[i]);         
-        command[commandType.name[i]].func(); 
+        // Serial.println("1111");
+        for (int indexStruct = 0; indexStruct < structSize; indexStruct++){
+          int commandResult = strcmp(command[indexStruct].name,commandType.name);   
+          // Serial.println("2222");
+          // Serial.println(commandResult);    
+          if (commandResult == 0){
+            command[indexStruct].func();       
+          }
+        }
+        commandLength = 0;
+        memset(commandType.name, NULL, BUFSIZE); //reset the array
+        // Serial.println(commandType.name);
+        // Serial.println("3333");
+        // token complete
        }
-        CHARCOUNT = 0;
-        memset(commandType.name, 0, BUFSIZE);
-      } // token complete
-      else 
-      {
-          CHARCOUNT++;
-          strncat(commandType.name,&c,1);
-          Serial.println(commandType.name);
-          
+      else{  
+        // Serial.println(commandLength);
+        if (commandLength > BUFSIZE){
+          Serial.println("command too long try again");
+          commandLength = 0;
+          memset(commandType.name, NULL, BUFSIZE); //reset the array           
+          processCommand();
+        }
+        else{
+        commandType.name[commandLength] = tolower(c);
+        commandLength++; 
+        }
       }// append c to buffer
     }
-  // do other stuff
+}
+
+void store(){
+  Serial.println("store function called");
+}
+void retrieve(){
+  Serial.println("retrieve function called");
+}
+void erase(){
+  Serial.println("erase function called");
+}
+void files(){
+  Serial.println("files function called");
+}
+void freespace(){
+  Serial.println("freespace function called");
+}
+void run(){
+  Serial.println("run function called");
+}
+void list(){
+  Serial.println("list function called");
+}
+void suspend(){
+  Serial.println("suspend function called");
+}
+void resume(){
+  Serial.println("resume function called");
+}
+void kill(){
+  Serial.println("kill function called");
 }
